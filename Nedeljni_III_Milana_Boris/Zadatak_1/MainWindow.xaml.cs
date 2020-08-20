3d4337e4822a13b2fa4e9ac2a69e94eb7feddc79
BorisP1996 <prposboris13@hotmail.com>
2020-08-20 11:19:05 +0200

    initial commit

---
+++ Nedeljni_III_Milana_Boris/Zadatak_1/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Zadatak_1.ViewModel;
+
+namespace Zadatak_1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = new MainWindowViewModel(this);
+        }
+
+        private void NumbersOnlyTextBox(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^0-9]+");
+            e.Handled = regex.IsMatch(e.Text);
+        }
+        private void LettersValidationTextBox(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^a-zA-Z]+");
+            e.Handled = regex.IsMatch(e.Text);
+        }
+    }
+}
---
+++ Nedeljni_III_Milana_Boris/Zadatak_1/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using Zadatak_1.Command;
+using Zadatak_1.Model;
+
+namespace Zadatak_1.ViewModel
+{
+    class MainWindowViewModel : ViewModelBase
+    {
+        MainWindow main;
+        Entity context = new Entity();
+
+        public MainWindowViewModel(MainWindow mainOpen)
+        {
+            main = mainOpen;
+        }
+
+        #region Properties
+        private string username;
+        public string Username
+        {
+            get
+            {
+                return username;
+            }
+            set
+            {
+                username = value;
+                OnPropertyChanged("Username");
+            }
+        }
+
+        private string password;
+        public string Password
+        {
+            get
+            {
+                return password;
+            }
+            set
+            {
+                password = value;
+                OnPropertyChanged("Password");
+            }
+        }
+        private string fullname;
+
+        public string FullName
+        {
+            get { return fullname; }
+            set
+            {
+                fullname = value;
+                OnPropertyChanged("FullName");
+            }
+        }
+
+        #endregion
+
+        #region Commands
+        private ICommand close;
+        public ICommand Close
+        {
+            get
+            {
+                if (close == null)
+                {
+                    close = new RelayCommand(param => CloseExecute(), param => CanCloseExecute());
+                }
+                return close;
+            }
+        }
+        private void CloseExecute()
+        {
+            main.Close();
+        }
+        private bool CanCloseExecute()
+        {
+            return true;
+        }
+
+        private ICommand login;
+        public ICommand Login
+        {
+            get
+            {
+                if (login == null)
+                {
+                    login = new RelayCommand(param => LoginExecute(), param => CanLoginExecute());
+                }
+                return login;
+            }
+        }
+        /// <summary>
+        /// Determins whos logged and opens new window according to that
+        /// </summary>
+        private void LoginExecute()
+        {
+            try
+            {
+                if (Username == "Admin" && Password == "Admin123")
+                {
+                    MessageBox.Show("Welcome admin");
+                }
+                else
+                {
+                    MessageBox.Show("Invalid parametres");
+                }
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.ToString());
+            }
+
+        }
+        private bool CanLoginExecute()
+        {
+            if (String.IsNullOrEmpty(Username) || String.IsNullOrEmpty(Password))
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+        #endregion
+    }
+}
